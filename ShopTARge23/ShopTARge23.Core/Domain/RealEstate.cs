c5f59df0b269a80e4f664cbd191a439915f16636
LAUREN\lauren <laurenpnn@gmail.com>
2024-10-08 13:05:21 +0300

    realestate

--- ShopTARge23/ShopTARge23.Core/Domain/RealEstate.cs
+++ ShopTARge23/ShopTARge23.Core/Domain/RealEstate.cs
@@ -1,14 +1,12 @@
-﻿namespace ShopTARge23.Core.Domain
-{
-    public class RealEstate
-    {
-        public Guid? Id { get; set; }
-        public double Size { get; set; }
-        public string Location { get; set; }
-        public int RoomNumber { get; set; }
-        public string BuildingType { get; set; }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-        public DateTime CreatedAt { get; set; }
-        public DateTime ModifiedAt { get; set; }
+namespace ShopTARge23.Core.Domain
+{
+    internal class RealEstate
+    {
     }
 }
--- ShopTARge23/ShopTARge23.Core/ServiceInterface/IRealEstateServices.cs
+++ ShopTARge23/ShopTARge23.Core/ServiceInterface/IRealEstateServices.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShopTARge23.Core.Dto
+namespace ShopTARge23.Core.ServiceInterface
 {
-    internal class RealEstateDto
+    internal class IRealEstateServices
     {
     }
 }
