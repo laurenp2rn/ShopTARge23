65a279a160b871b4c092fa910e9ee0fc5c685f73
LAUREN\lauren <laurenpnn@gmail.com>
2024-10-28 09:47:52 +0200

    kindergarteni korda tegemine

--- ShopTARge23/ShopTARge23.Core/Dto/KindergartenDto.cs
+++ ShopTARge23/ShopTARge23.Core/Dto/KindergartenDto.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,20 +6,7 @@ using System.Threading.Tasks;
 
 namespace ShopTARge23.Core.Dto
 {
-    public class KindergartenDto
+    internal class KindergartenDto
     {
-        public Guid? Id { get; set; }
-        public string GroupName { get; set; }
-        public int ChildrenCount { get; set; }
-        public string KindergartenName { get; set; }
-        public string Teacher { get; set; }
-
-        public List<IFormFile> Files { get; set; }
-        public IEnumerable<FileToDatabaseDto> Image { get; set; }
-            = new List<FileToDatabaseDto>();
-
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-
     }
 }
